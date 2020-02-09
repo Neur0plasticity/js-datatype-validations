fb705e8bf6f38acde6bdfb1c89647edcd16ef1a6
djtinkers <djtinkers365@gmail.com>
2020-02-09 10:01:37 -0500

    outlined code

---
+++ main.ts
@@ -0,0 +1,59 @@
+export const dType = function(){
+
+};
+const datatypes = [
+    "boolean",
+    "number",
+    "string",
+    "object",
+    "array",
+    "symbol",
+    "undefined",
+    "null",
+    "function",
+    "class",
+];
+let funcsOBJ = {};
+
+const boolean = Object.assign((v => typeof v === "boolean"),
+        {
+
+        }
+);
+const number = Object.assign((v => typeof v === "number"),
+        {
+
+        }
+);
+const string = Object.assign((v => typeof v === "string"),
+        {
+
+        }
+);
+const object = Object.assign((v => typeof v === "number"),
+        {
+
+        }
+);
+const array = Object.assign((v => typeof v === "number"),
+        {
+
+        }
+);
+const symbol = Object.assign((v => typeof v === "number"),
+        {
+
+        }
+);
+const function = Object.assign((v => typeof v === "number"),
+        {
+
+        }
+);
+const class = Object.assign((v => typeof v === "number"),
+        {
+
+        }
+);
+
+
