59825ca88d955e93866b1821cd377b408fcb68e7
djtinkers <djtinkers365@gmail.com>
2020-02-10 11:44:33 -0500

    almost finished

--- main.ts
+++ main.ts
@@ -7,44 +7,80 @@ const _dType = function(func,obj){
     }
     return Object.assign(func,obj);
 };
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+const _lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","y","z"];
+const _upperCase = _lowerCase.map(v => v.toUpperCase());
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+const isTrue     =   v => v === true;
+const isFalse    =   v => v === false;
+
+const isSafe     =   v => Number.isSafeInteger(v);
+const isInfinity =   v => v === Infinity;
+const isFinite   =   v => Number.isFinite(v);
+const isMaxValue =   v => v === Number.MAX_VALUE;
+const isNaN      =   v => Number.isNaN(v);
+const isPositive =   v => 0 < v;
+const isNegative =   v => 0 > v;
+const isZero     =   v => v === 0;
+const isFloat    =   v => String(v).includes(".");
+const isAlpha    =   v => [..._lowerCase;..._upperCase].includes(v);
+const isThis     =   v => v.name;
+const isAnon     =   v => v;
+const isLambda   =   v => v;
+
+const isEmpty = (v)=>{
+    return {
+    "string":  v => v.length === 0,
+    "object":  v => Object.keys(v).length === 0,
+    "array" :  v => v.length === 0,
+    "function":v => v.toString()
+    }[(Array.isArray(v) && "array") || (typeof v)](v);
+};
+const isNotEmpty = (v)=>{
+    return {
+        "string":  v => v.length !== 0,
+        "object":  v => Object.keys(v).length !== 0,
+        "array" :  v => v.length !== 0,
+        "function":v => v.toString()
+    }[(Array.isArray(v) && "array") || (typeof v)](v);
+};
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 const _boolean = [(v => typeof v === "boolean"),
         {
-            isTrue:      v => v === true,
-            isFalse:     v => v === false
+            isTrue,     isFalse
         }
 ];
 const _number = [(v => typeof v === "number"),
         {
-            isSafe:       v => Number.isSafeInteger(v),
-            isInfinity:   v => v === Infinity,
-            isFinite:     v => Number.isFinite(v),
-            isMaxValue:   v => v === Number.MAX_VALUE,
-            isNaN:        v => Number.isNaN(v),
-            isPositive:   v => 0 < v,
-            isNegative:   v => 0 > v,
-            isZero:       v => v === 0,
-            isFloat:      v => String(v).includes(".")
+            isSafe,
+            isInfinity, isFinite,
+            isMaxValue, isMinValue,
+            isZero,     isNaN,
+            isPositive, isNegative,
+            isInteger,  isFloat
         }
 ];
-const _lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","y","z"];
-const _upperCase = _lowerCase.map(v => v.toUpperCase());
 const _string = [(v => typeof v === "string"),
         {
-            isEmpty:      v => v.length === 0,
-            isNotEmpty:   v => v.length !== 0,
-            isAlpha:      v => [..._lowerCase,..._upperCase].includes(v),
+            isEmpty,    isNotEmpty,
+            isAlpha,    isSymbol,
+            isLowerCase,isUpperCase 
         }
 ];
 const _object = [(v => !Array.isArray(v) && typeof v === "object"),
         {
-            isEmpty:      v => Object.keys(v).length === 0,
-            isNotEmpty:   v => Object.keys(v).length !== 0             
+            isEmpty,    isNotEmpty
         }
 ];
 const _array = [(v => Array.isArray(v)),
         {
-            isEmpty:      v => v.length === 0,
-            isNotEmpty:   v => v.length !== 0
+            isEmpty,    isNotEmpty
         }
 ];
 const _symbol = [(v => typeof v === "symbol"),
@@ -54,21 +90,20 @@ const _symbol = [(v => typeof v === "symbol"),
 ];
 const _function = [(v => typeof v === "function"),
         {
-            isThis:       v => v.name,
-            isAnon:       v => v,
-            isLambda:     v => v,
-            isEmpty:      v => v.toString().
+            isThis,     isAnon,     isLambda,
+            isEmpty,
         }
 ];
 const _class = [(v => typeof v === "function"),
         {
             isEmpty,
-            
         }
 ];
-const _undefined = [];
-const _null = [];
-
+const _undefined = [(v => v === undefined)];
+const _null = [(v => v === null)];
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 export const DType = Object.freeze({
     _boolean:       _dType(_boolean[0]  ,_boolean[1]),
     _number:        _dType(_number[0]   ,_number[1]),
